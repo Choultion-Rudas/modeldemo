6710d6b242c6126d9e76d75207f2fae56c999a1b
Choultion-Rudas <73950428+Choultion-Rudas@users.noreply.github.com>
2025-10-26 10:49:29 +0800

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -1,7 +1,6 @@
 export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
-
 		if (url.pathname.startsWith('/api/')) {
 			if (request.method === 'OPTIONS') {
 				return new Response(null, {
@@ -15,30 +14,18 @@ export default {
 					}
 				});
 			}
-
-			const target = new URL(
-				url.pathname.replace(/^\/api/, '') + url.search,
-				'https://choultion-rudas-sensevoice.hf.space'
-			);
-
+			const target = new URL(url.pathname.replace(/^\/api/, '') + url.search, 'https://choultion-rudas-sensevoice.hf.space');
 			const upstream = await fetch(target.toString(), {
 				method: request.method,
 				headers: request.headers,
 				body: (request.method === 'GET' || request.method === 'HEAD') ? undefined : request.body,
 				redirect: 'follow'
 			});
-
 			const h = new Headers(upstream.headers);
 			h.set('Access-Control-Allow-Origin', '*');
 			h.append('Vary', 'Origin');
-
-			return new Response(upstream.body, {
-				status: upstream.status,
-				statusText: upstream.statusText,
-				headers: h
-			});
+			return new Response(upstream.body, { status: upstream.status, statusText: upstream.statusText, headers: h });
 		}
-
 		return env.ASSETS.fetch(request);
 	}
 };
